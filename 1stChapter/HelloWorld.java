d2249cb12677b3f2c3510bc368b87bdb6c08a8c1
tianik909 <anik909@hotmail.cooom>
2016-07-04 10:58:40 +0600

    HelloWorld
    1st Chapter exercise

---
+++ 1stChapter/HelloWorld.java
@@ -0,0 +1,20 @@
+/**************************************************
+* Author : Anik Iqbal                             *
+* Date   : July 4 , 2016                          *
+* Mood   : Not that good                          *
+***************************************************/
+
+public class HelloWorld{
+
+	public static void main(String[] args){
+
+	System.out.println("Hello World!\n");
+        // "\n" indicates new line character 
+
+	
+	}
+
+
+
+
+}
